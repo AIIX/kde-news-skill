bdc3928663c20ac8f36f9a3b349e28548c1b42dd
Aditya Mehra <Aix.m@outlook.com>
2018-06-19 21:40:11 +0930

    Upload Fixed Version

--- __init__.py
+++ __init__.py
@@ -11,6 +11,7 @@ from mycroft.skills.context import *
 from mycroft.util import read_stripped_lines
 from mycroft.util.log import getLogger
 from mycroft.messagebus.message import Message
+from word2number import w2n
 
 __author__ = 'aix'
 
@@ -20,18 +21,42 @@ class KDENewsSkill(MycroftSkill):
     def __init__(self):
         super(KDENewsSkill, self).__init__(name="KDENewsSkill")
 
-    @intent_handler(IntentBuilder("GetLatestNews").require("GetLatestKeyword").build())
-    def handle_get_latestnews_intent(self, message):
+    @intent_handler(IntentBuilder("WhatsNewAtKde").require("WhatsNewKeyword").build())
+    def handle_whatsnewatkde_intent(self, message):
         method = "GET"
         url = "https://api.rss2json.com/v1/api.json?rss_url=https://dot.kde.org/rss.xml"
         response = requests.request(method,url)
+        global articleList
+        global articleUrlList
+        global articleDescList
+        articleList = []
+        articleDescList = []
+        articleUrlList = []
+        articleThumbList = []
         data = response.json()
-        currentTitle = data['items'][0]['title']
-        currentDesc = data['items'][0]['description']
-        soup = BeautifulSoup(currentDesc)
-        self.speak(currentTitle)
-        self.speak(soup.get_text())
-        self.enclosure.ws.emit(Message("kdenewsObject", {'desktop': {'data': response.text}}))
+        for item in data['items']:
+            currentTitle = item['title']
+            currentUrl = item['link']
+            currentThumb = item['thumbnail']
+            currentDesc = item['description']
+            soup = BeautifulSoup(currentDesc)
+            articleList.append(currentTitle)
+            articleUrlList.append(currentUrl)
+            articleThumbList.append(currentThumb)
+            articleDescList.append(soup.get_text())
+        for index,item in enumerate(articleList):
+            speakData = "Article {0}: {1}".format(str(index+1), item)
+            self.speak(speakData)
+        self.enclosure.ws.emit(Message("kdenewsObject", {'desktop': {'data': {'titlelist': articleList, 'urllist': articleUrlList, 'thumb': articleThumbList}}}))
+    
+    @intent_handler(IntentBuilder("GetArticleDesc").require("ArticleNumberKeyword").build())
+    def handle_getarticledesc_intent(self, message):
+        utterance = message.data.get('utterance').lower()
+        utterance = utterance.replace(message.data.get('ArticleNumberKeyword'), '')
+        searchString = utterance
+        getArticleVal = w2n.word_to_num(searchString)
+        articlefetchindex = articleDescList[int(getArticleVal)-1]
+        self.speak(articlefetchindex)
         
     def stop(self):
         pass
